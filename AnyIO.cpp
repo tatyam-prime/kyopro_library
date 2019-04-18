4ae2de8dbf5988aed37ce24f2b37c260075d6e49
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2019-04-18 23:29:07 +0900

    Update AnyIO.cpp

--- AnyIO.cpp
+++ AnyIO.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 inline int scan(){ return getchar(); }
-inline void scan(bool &a){ int b; scanf("%d", &b); a=b; }
+inline void scan(vector<bool>::reference &a){ int b; scanf("%d", &b); a = b; }
 inline void scan(int &a){ scanf("%d", &a); }
 inline void scan(unsigned &a){ scanf("%u", &a); }
 inline void scan(long &a){ scanf("%ld", &a); }
@@ -13,6 +13,7 @@ inline void scan(char &a){ scanf("%c", &a); }
 inline void scan(float &a){ scanf("%f", &a); }
 inline void scan(double &a){ scanf("%lf", &a); }
 inline void scan(long double &a){ scanf("%Lf", &a); }
+inline void scan(vector<bool> &vec){ for(unsigned i = 0; i < vec.size(); i++) { int a; scan(a); vec[i] = a; } }
 template<class T> inline void scan(vector<T> &vec);
 template<class T, size_t size> inline void scan(array<T, size> &vec);
 template<class T, class L> inline void scan(pair<T, L> &p);
@@ -41,10 +42,10 @@ template<class T, size_t size> void print(const array<T, size> &vec);
 template<class T, class L> void print(const pair<T, L> &p);
 template<class T, size_t size> inline enable_if_t<is_same<T, const char>::value == 0> print(const T (&vec)[size]);
 template<class T> void print(const vector<T> &vec){ print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
-template<class T, size_t size> void print(const array<T, size> &vec){ print(vec[0]); for(auto i=vec.begin(); ++i!=vec.end(); ){ putchar(' '); print(*i); } }
+template<class T, size_t size> void print(const array<T, size> &vec){ print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
 template<class T, class L> void print(const pair<T, L> &p){ print(p.first); putchar(' '); print(p.second); }
-template<class T, size_t size> inline enable_if_t<is_same<T, const char>::value == 0> print(const T (&vec)[size]){ print(vec[0]); for(auto i=vec; ++i != end(vec); ){ putchar(' '); print(*i); } }
-template<class T> inline void print(const T &a){ cout<<a; }
+template<class T, size_t size> inline enable_if_t<is_same<T, const char>::value == 0> print(const T (&vec)[size]){ print(vec[0]); for(auto i = vec; ++i != end(vec); ){ putchar(' '); print(*i); } }
+template<class T> inline void print(const T &a){ cout << a; }
 inline bool out(){ putchar('\n'); return 0; }
 template<class T> inline bool out(const T &t){ print(t); putchar('\n'); return 0; }
 template<class Head, class... Tail> inline bool out(const Head &head, const Tail&... tail){ print(head); print(' '); out(tail...); return 0; }
