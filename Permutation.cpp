8d30764480e40efa877a8e91a5e7e42a9ad507ee
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2019-07-12 22:13:21 +0900

    Update Permutation.cpp

--- Permutation.cpp
+++ Permutation.cpp
@@ -7,9 +7,9 @@ struct Permutation { // 0-indexed
     template<class T> Permutation(const initializer_list<T>& perm): perm(perm.begin(), perm.end()) {}
     template<class T> Permutation(const vector<T>& perm): perm(perm.begin(), perm.end()) {}
     static Permutation identity(ll n) { vector<ll> ans(n); iota(ans.begin(), ans.end(), 0); return ans; }
-    Permutation inv() const { vector<ll> ans(perm.size()); rep(perm.size()) ans[perm[i]] = i; return ans; }
-    Permutation operator* (const Permutation& x) const { vector<ll> ans(perm.size()); rep(perm.size()) ans[i] = perm[x.perm[i]]; return ans; }
-    Permutation& operator*=(const Permutation& x) { vector<ll> ans(perm.size()); rep(perm.size()) ans[i] = perm[x.perm[i]]; perm.swap(ans); return *this; }
+    Permutation inv() const { vector<ll> ans(perm.size()); for(ll i = 0; i < perm.size(); i++) ans[perm[i]] = i; return ans; }
+    Permutation operator* (const Permutation& x) const { vector<ll> ans(perm.size()); for(ll i = 0; i < perm.size(); i++) ans[i] = perm[x.perm[i]]; return ans; }
+    Permutation& operator*=(const Permutation& x) { vector<ll> ans(perm.size()); for(ll i = 0; i < perm.size(); i++) ans[i] = perm[x.perm[i]]; perm.swap(ans); return *this; }
     Permutation pow(ll x) const {
         Permutation ans = identity(perm.size()), cnt = *this;
         while(x){
