493840490830aeb5b1b088e8abccff45179400a3
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2019-07-21 21:42:02 +0900

    Update AnyIO.cpp

--- AnyIO.cpp
+++ AnyIO.cpp
@@ -40,7 +40,7 @@ template<class T> void print(const vector<T> &vec);
 template<class T, size_t size> void print(const array<T, size> &vec);
 template<class T, class L> void print(const pair<T, L> &p);
 template<class T, size_t size> inline void print(const T (&vec)[size]);
-template<class T> void print(const vector<T> &vec){ print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
+template<class T> void print(const vector<T> &vec){ if(vec.empty()) return; print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
 template<class T, size_t size> void print(const array<T, size> &vec){ print(vec[0]); for(auto i = vec.begin(); ++i != vec.end(); ){ putchar(' '); print(*i); } }
 template<class T, class L> void print(const pair<T, L> &p){ print(p.first); putchar(' '); print(p.second); }
 template<class T, size_t size> inline void print(const T (&vec)[size]){ print(vec[0]); for(auto i = vec; ++i != end(vec); ){ putchar(' '); print(*i); } }
