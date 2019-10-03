d1f61c261c3cdb93c76e3e3f8e9b1ada3c925057
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2019-10-03 22:06:46 +0900

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -62,3 +62,32 @@ struct SegmentTreeSum : SegmentTree<T>{
     SegmentTreeSum(ll n, const T& def_value = T()) : SegmentTree<T>(n, def_value, [](T a, T b){return a + b;}){}
     SegmentTreeSum(const vector<T>& v, const T& def_value = T()) : SegmentTree<T>(v, def_value, [](T a, T b){return a + b;}){}
 };
+
+
+// easy version
+
+struct SegmentTree{
+    ll size = 1;
+    vector<ll> data;
+    SegmentTree(ll n){
+        while(size < n) size *= 2;
+        data.assign(size * 2, -LINF);
+    }
+    void update(ll at){
+        while(at /= 2) data[at] = max(data[at * 2], data[at * 2 + 1]);
+    }
+    void set(ll at, ll val){
+        at += size;
+        data[at] = val;
+        update(at);
+    }
+    ll get(ll l, ll r){
+        ll ans = -LINF;
+        l += size; r += size;
+        for(; l < r; l /= 2, r /= 2){
+            if(l & 1) chmax(ans, data[l++]);
+            if(r & 1) chmax(ans, data[--r]);
+        }
+        return ans;
+    }
+};
