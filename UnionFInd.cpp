338970094d9825c04d1d97a28bdaf0a36856b0b7
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2019-10-03 21:58:25 +0900

    Create UnionFInd.cpp

---
+++ UnionFInd.cpp
@@ -0,0 +1,18 @@
+struct UnionFind{
+    vector<ll>data;
+    UnionFind(ll n):data(n,-1){}
+    bool Union(ll a,ll b){
+        a=root(a);b=root(b);
+        if(a==b)return 1;
+        if(data[a]>data[b])swap(a,b);
+        data[a]+=data[b];
+        data[b]=a;
+        return 0;
+    }
+    bool Find(ll a,ll b){return root(a)==root(b);}
+    ll root(ll a){return data[a]<0?a:data[a]=root(data[a]);}
+    //* extra
+    ll size(ll a){return -data[root(a)];}
+    ll operator[](ll a){return root(a);}
+    //*/
+};
