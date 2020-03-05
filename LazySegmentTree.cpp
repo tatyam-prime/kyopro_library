2541e74d20e23669a04bc3804945b91192671a13
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2020-03-06 02:10:29 +0900

    Update LazySegmentTree.cpp

--- LazySegmentTree.cpp
+++ LazySegmentTree.cpp
@@ -138,3 +138,91 @@ struct RUQRSQ : LazySegmentTree<T, T>{
         for(ll i = Base::size; --i;) Base::data[i] = f(Base::data[i * 2], Base::data[i * 2 + 1]);
     }
 };
+
+
+
+
+// -----------------   双対セグメント木  --------------------------
+
+template<class T> bool chmin(T& a, const T& b){ if(a > b){ a = b; return 1; } return 0; }
+
+
+
+
+template<class T>
+struct DualSegmentTree{
+    virtual void c(T&, const T&) = 0;
+    ll size = 1, rank = 0;
+    vector<T> lazy;
+    const T def_lazy;
+    DualSegmentTree(ll n, const T& def_value, const T& def_lazy): def_lazy(def_lazy){
+        while(size < n){
+            size *= 2;
+            rank++;
+        }
+        lazy.assign(size * 2, def_lazy);
+        for(ll i = size; i < size * 2; i++) lazy[i] = def_value;
+    }
+    DualSegmentTree(const vector<T>& v, const T& def_lazy): def_lazy(def_lazy){
+        while(size < v.size()){
+            size *= 2;
+            rank++;
+        }
+        lazy.assign(size * 2, def_lazy);
+        for(ll i = 0; i < v.size(); i++) lazy[size + i] = v[i];
+    }
+    void push(ll at){
+        if(!at) return;
+        ll r = 31 - __builtin_clz(at);
+        for(ll i = r; i > 0; i--){
+            ll a = at >> i;
+            if(lazy[a] != def_lazy){
+                c(lazy[a * 2], lazy[a]);
+                c(lazy[a * 2 + 1], lazy[a]);
+                lazy[a] = def_lazy;
+            }
+        }
+    }
+    T operator[](ll at){
+        at += size;
+        push(at);
+        return lazy[at];
+    }
+    void set(ll at, const T& val){
+        at += size;
+        push(at);
+        lazy[at] = val;
+    }
+    void query(ll l, ll r, const T& val){
+        if(l >= r) return;
+        l += size;
+        r += size;
+        push(l >> __builtin_ctz(l));
+        push((r >> __builtin_ctz(r)) - 1);
+        for(; l < r; l /= 2, r /= 2){
+            if(l & 1) c(lazy[l++], val);
+            if(r & 1) c(lazy[--r], val);
+        }
+    }
+};
+template<class T>
+struct RAQ : DualSegmentTree<T>{
+    using Base = DualSegmentTree<T>;
+    void c(T& a, const T& b){ a += b; }
+    RAQ(ll n, const T& def_value = T(), const T& def_lazy = T()) : Base(n, def_value, def_lazy){}
+    RAQ(const vector<T>& v, const T& def_lazy = T()) : Base(v, def_lazy){}
+};
+template<class T>
+struct RUQ : DualSegmentTree<T>{
+    using Base = DualSegmentTree<T>;
+    void c(T& a, const T& b){ a = b; }
+    RUQ(ll n, const T& def_value, const T& def_lazy = numeric_limits<T>::max()) : Base(n, def_value, def_lazy){}
+    RUQ(const vector<T>& v, const T& def_lazy = numeric_limits<T>::max()) : Base(v, def_lazy){}
+};
+template<class T>
+struct RchmQ : DualSegmentTree<T>{
+    using Base = DualSegmentTree<T>;
+    void c(T& a, const T& b){ chmin(a, b); }
+    RchmQ(ll n, const T& def_value, const T& def_lazy = numeric_limits<T>::max()) : Base(n, def_value, def_lazy){}
+    RchmQ(const vector<T>& v, const T& def_lazy = numeric_limits<T>::max()) : Base(v, def_lazy){}
+};
