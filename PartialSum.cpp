4ac3ea9ac137b4b68f2a7715581fe0d250eca740
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2020-05-01 22:24:30 +0900

    Update PartialSum.cpp

--- PartialSum.cpp
+++ PartialSum.cpp
@@ -2,25 +2,25 @@ using namespace std;
 using ll = long long;
 
 
-struct PartialSum{
-    vector<ll> data;
-    template<class T> PartialSum(const T& v): data(v.size() + 1){
+template<class T> struct PartialSum{
+    vector<T> data;
+    template<class U> PartialSum(const U& v): data(v.size() + 1){
         for(ll i = 0; i < v.size(); i++) data[i + 1] = data[i] + v[i];
     }
-    ll get(ll l, ll r) const {
+    T get(ll l, ll r) const {
         return data[r] - data[l];
     }
-    ll operator[](ll at) const { return data[at]; }
+    T operator[](ll at) const { return data[at]; }
 };
 
-struct PartialSum2D{
-    vector<vector<ll>> data;
-    template<class T> PartialSum2D(const T& v): data(v.size() + 1, vector<ll>(v[0].size() + 1)){
+template<class T> struct PartialSum2D{
+    vector<vector<T>> data;
+    template<class U> PartialSum2D(const T& v): data(v.size() + 1, vector<T>(v[0].size() + 1)){
         for(ll i = 0; i < v.size(); i++) for(ll j = 0; j < v[i].size(); j++) data[i + 1][j + 1] = data[i][j + 1] + v[i][j];
         for(ll i = 0; i < v.size(); i++) for(ll j = 0; j < v[i].size(); j++) data[i + 1][j + 1] += data[i + 1][j];
     }
-    ll get(ll x1, ll x2, ll y1, ll y2) const {
+    T get(ll x1, ll x2, ll y1, ll y2) const {
         return data[x2][y2] + data[x1][y1] - data[x1][y2] - data[x2][y1];
     }
-    const vector<ll>& operator[](ll at) const { return data[at]; }
+    const vector<T>& operator[](ll at) const { return data[at]; }
 };
