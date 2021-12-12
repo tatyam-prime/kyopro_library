e7beecf8775358f2d9cac863791486aaeeb33847
tatyam-prime <40888698+tatyam-prime@users.noreply.github.com>
2021-12-13 03:23:10 +0900

    Create fast_graph.cpp

---
+++ fast_graph.cpp
@@ -0,0 +1,34 @@
+struct fast_graph{
+    using I = vector<int>::iterator;
+    struct edges{
+        I l, r;
+        bool empty() const { return l == r; }
+        size_t size() const { return r - l; }
+        auto begin() const { return l; }
+        auto end() const { return r; }
+        int operator[](ssize_t i) const { return l[i]; }
+    };
+    
+    vector<int> mem;
+    vector<edges> g;
+    fast_graph(){}
+    fast_graph(int n, vector<pair<int, int>> edge): g(n), mem(edge.size() * 2){
+        for(auto [a, b] : edge){
+            g[a].r++;
+            g[b].r++;
+        }
+        I sum = mem.begin();
+        for(int i = 0; i < n; i++){
+            tie(g[i].l, g[i].r, sum) = tuple{sum, sum, sum + (g[i].r - g[i].l)};
+        }
+        for(auto [a, b] : edge){
+            *g[a].r++ = b;
+            *g[b].r++ = a;
+        }
+    }
+    bool empty() const { return g.empty(); }
+    size_t size() const { return g.size(); }
+    auto begin() const { return g.begin(); }
+    auto end() const { return g.end(); }
+    const auto& operator[](ssize_t i) const { return g[i]; }
+};
