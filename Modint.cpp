15f7afc6f9ce01e3ea847b1f967e2ffdbb451876
tatyam <40888698+tatyam-prime@users.noreply.github.com>
2023-03-01 16:38:38 +0900

    Update Modint.cpp

--- Modint.cpp
+++ Modint.cpp
@@ -2,42 +2,41 @@ using ll = long long;
 using ull = unsigned long long;
 
 
-constexpr unsigned mod = 1000000007;
+constexpr uint mod = 998244353;
 struct Modint{
-    unsigned num = 0;
+    uint num = 0;
     constexpr Modint() noexcept {}
     constexpr Modint(const Modint &x) noexcept : num(x.num){}
-    inline constexpr operator ll() const noexcept { return num; }
-    inline constexpr Modint& operator+=(Modint x) noexcept { num += x.num; if(num >= mod) num -= mod; return *this; }
-    inline constexpr Modint& operator++() noexcept { if(num == mod - 1) num = 0; else num++; return *this; }
-    inline constexpr Modint operator++(int) noexcept { Modint ans(*this); operator++(); return ans; }
-    inline constexpr Modint operator-() const noexcept { return Modint(0) -= *this; }
-    inline constexpr Modint operator-(Modint x) const noexcept { return Modint(*this) -= x; }
-    inline constexpr Modint& operator-=(Modint x) noexcept { if(num < x.num) num += mod; num -= x.num; return *this; }
-    inline constexpr Modint& operator--() noexcept { if(num == 0) num = mod - 1; else num--; return *this; }
-    inline constexpr Modint operator--(int) noexcept { Modint ans(*this); operator--(); return ans; }
-    inline constexpr Modint& operator*=(Modint x) noexcept { num = ull(num) * x.num % mod; return *this; }
-    inline constexpr Modint& operator/=(Modint x) noexcept { return operator*=(x.inv()); }
+    constexpr operator ll() const noexcept { return num; }
+    constexpr Modint& operator+=(Modint x) noexcept { num += x.num; if(num >= mod) num -= mod; return *this; }
+    constexpr Modint& operator++() noexcept { if(num == mod - 1) num = 0; else num++; return *this; }
+    constexpr Modint operator++(int) noexcept { Modint ans(*this); operator++(); return ans; }
+    constexpr Modint operator-() const noexcept { return Modint(0) -= *this; }
+    constexpr Modint& operator-=(Modint x) noexcept { if(num < x.num) num += mod; num -= x.num; return *this; }
+    constexpr Modint& operator--() noexcept { if(num == 0) num = mod - 1; else num--; return *this; }
+    constexpr Modint operator--(int) noexcept { Modint ans(*this); operator--(); return ans; }
+    constexpr Modint& operator*=(Modint x) noexcept { num = ull(num) * x.num % mod; return *this; }
+    constexpr Modint& operator/=(Modint x) noexcept { return operator*=(x.inv()); }
     template<class T> constexpr Modint(T x) noexcept {
         using U = typename conditional<sizeof(T) >= 4, T, int>::type;
-        U y = x; y %= U(mod); if(y < 0) y += mod; num = unsigned(y);
+        U y = x; y %= U(mod); if(y < 0) y += mod; num = uint(y);
     }
-    template<class T> inline constexpr Modint operator+(T x) const noexcept { return Modint(*this) += x; }
-    template<class T> inline constexpr Modint& operator+=(T x) noexcept { return operator+=(Modint(x)); }
-    template<class T> inline constexpr Modint operator-(T x) const noexcept { return Modint(*this) -= x; }
-    template<class T> inline constexpr Modint& operator-=(T x) noexcept { return operator-=(Modint(x)); }
-    template<class T> inline constexpr Modint operator*(T x) const noexcept { return Modint(*this) *= x; }
-    template<class T> inline constexpr Modint& operator*=(T x) noexcept { return operator*=(Modint(x)); }
-    template<class T> inline constexpr Modint operator/(T x) const noexcept { return Modint(*this) /= x; }
-    template<class T> inline constexpr Modint& operator/=(T x) noexcept { return operator/=(Modint(x)); }
-    inline constexpr Modint inv() const noexcept { ll x = 0, y = 0; extgcd(num, mod, x, y); return x; }
-    static inline constexpr ll extgcd(ll a, ll b, ll &x, ll &y) noexcept { ll g = a; x = 1; y = 0; if(b){ g = extgcd(b, a % b, y, x); y -= a / b * x; } return g; }
-    inline constexpr Modint pow(ull x) const noexcept { Modint ans = 1, cnt = *this; while(x){ if(x & 1) ans *= cnt; cnt *= cnt; x /= 2; } return ans; }
+    template<class T> constexpr Modint operator+(T x) const noexcept { return Modint(*this) += x; }
+    template<class T> constexpr Modint& operator+=(T x) noexcept { return operator+=(Modint(x)); }
+    template<class T> constexpr Modint operator-(T x) const noexcept { return Modint(*this) -= x; }
+    template<class T> constexpr Modint& operator-=(T x) noexcept { return operator-=(Modint(x)); }
+    template<class T> constexpr Modint operator*(T x) const noexcept { return Modint(*this) *= x; }
+    template<class T> constexpr Modint& operator*=(T x) noexcept { return operator*=(Modint(x)); }
+    template<class T> constexpr Modint operator/(T x) const noexcept { return Modint(*this) /= x; }
+    template<class T> constexpr Modint& operator/=(T x) noexcept { return operator/=(Modint(x)); }
+    constexpr Modint inv() const noexcept { ll x = 0, y = 0; extgcd(num, mod, x, y); return x; }
+    static constexpr ll extgcd(ll a, ll b, ll &x, ll &y) noexcept { ll g = a; x = 1; y = 0; if(b){ g = extgcd(b, a % b, y, x); y -= a / b * x; } return g; }
+    constexpr Modint pow(ull x) const noexcept { Modint ans = 1, cnt = *this; while(x){ if(x & 1) ans *= cnt; cnt *= cnt; x /= 2; } return ans; }
 };
-std::istream& operator>>(std::istream& is, Modint& x) noexcept { ll a; cin >> a; x = a; return is; }
-inline constexpr Modint operator""_M(ull x) noexcept { return Modint(x); }
+std::istream& operator>>(std::istream& is, Modint& x){ ll a; in(a); x = a; return is; }
+constexpr Modint operator""_M(ull x) noexcept { return Modint(x); }
 std::vector<Modint> fac(1, 1), inv(1, 1);
-inline void reserve(ll a){
+void reserve(ll a){
     if(fac.size() >= a) return;
     if(a < fac.size() * 2) a = fac.size() * 2;
     if(a >= mod) a = mod;
@@ -46,15 +45,15 @@ inline void reserve(ll a){
     inv.back() = fac.back().inv();
     for(ll i = inv.size() - 1; !inv[i - 1]; i--) inv[i - 1] = inv[i] * i;
 }
-inline Modint fact(ll n){ if(n < 0) return 0; reserve(n + 1); return fac[n]; }
-inline Modint perm(ll n, ll r){
+Modint fact(ll n){ if(n < 0) return 0; reserve(n + 1); return fac[n]; }
+Modint perm(ll n, ll r){
     if(r < 0 || n < r) return 0;
     if(n >> 24){ Modint ans = 1; for(ll i = 0; i < r; i++) ans *= n--; return ans; }
     reserve(n + 1); return fac[n] * inv[n - r];
 }
-inline Modint comb(ll n, ll r){ if(r < 0 || n < r) return 0; reserve(r + 1); return perm(n, r) * inv[r]; }
-inline Modint Mcomb(ll n, ll r){ return comb(n + r - 1, n - 1); } // r個をn部屋に分ける
-inline Modint catalan(ll n){ reserve(n * 2 + 1); return fac[n * 2] * inv[n] * inv[n + 1]; }
+Modint comb(ll n, ll r){ if(r < 0 || n < r) return 0; r = min(r, n - r); reserve(r + 1); const Modint x = inv[r]; return perm(n, r) * x; }
+Modint Mcomb(ll n, ll r){ return comb(n + r - 1, r); } // r balls into n boxes
+Modint catalan(ll n){ reserve(n * 2 + 1); return fac[n * 2] * inv[n] * inv[n + 1]; }
 
 //　exawizards2019_e
 int main(){
